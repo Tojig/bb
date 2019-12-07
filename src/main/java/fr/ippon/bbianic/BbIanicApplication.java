42a1c38415a9d5a6813ed66fe110031f95ecca47
Ianic de Souza Almeida <idesouza@ippon.fr>
2019-12-07 14:53:16 +0100

    project initializr

---
+++ src/main/java/fr/ippon/bbianic/BbIanicApplication.java
@@ -0,0 +1,24 @@
+package fr.ippon.bbianic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BbIanicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbIanicApplication.class, args);
+	}
+
+
+	@RestController
+	private static class Controller {
+
+		@GetMapping("/hello")
+		public String getHello() {
+			return "Hello 1.0";
+		}
+	}
+}
---
+++ src/test/java/fr/ippon/bbianic/BbIanicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ippon.bbianic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbIanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
